ee619e4329ba387e55c1ebd2ec7951f5279f0c8b
JohnDan14 <yonadan14@gmail.com>
2023-01-20 19:30:41 +0200

    added 11

---
+++ p11-318416112-323817627/SymbolTable.py
@@ -0,0 +1,60 @@
+class SymbolTable:
+    def __init__(self):
+        self.class_scope = {}
+        self.subroutines_scope = {}
+        self.current_scope = self.class_scope 
+        self.counters_dict = {
+            'static': 0,
+            'field': 0,
+            'arg': 0,
+            'var': 0,
+            'if': 0,
+            'while': 0
+        }
+    
+    def reset(self):
+        for name in self.counters_dict:
+            if name not in ['static', 'field']:
+                self.counters_dict[name] = 0
+
+    def add_subroutine(self, subr_name):
+        self.subroutines_scope[subr_name] = {}
+
+    def define(self, name, type, kind):
+        if kind in ['static', 'field']:
+            self.class_scope[name] = (type, kind, self.counters_dict[name])
+            self.counters_dict[name] += 1
+        elif kind in ['arg', 'var']:
+            self.current_scope[name] = (type, kind, self.counters_dict[name])
+            self.counters_dict[name] += 1
+    
+    def var_count(self, kind):
+        return len([tuple for (name, tuple) in self.current_scope.items() if tuple[1] == kind])
+
+    def kind_of(self, name):
+        if name in self.current_scope:
+            return self.current_scope[name][1]
+        elif name in self.class_scope:
+            return self.class_scope[name][1]
+        else:
+            return 'NONE'        
+
+    def type_of(self, name):
+        if name in self.current_scope:
+            return self.current_scope[name][0]
+        elif name in self.class_scope:
+            return self.class_scope[name][0]
+
+    def index_of(self, name):
+        if name in self.current_scope:
+            return self.current_scope[name][2]
+        elif name in self.class_scope:
+            return self.class_scope[name][2]
+
+    def set_scope(self, subr_name):
+        if subr_name == 'class':
+            self.current_scope = self.class_scope
+        else:
+            self.current_scope = self.subroutines_scope[subr_name]                
+
+        
